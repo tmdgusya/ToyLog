9ad44972d84cb57cec41420f1c6ee5be86e4cf47
tmdgusya <tmdgusya@naver.com>
2021-03-25 20:18:24 +0900

    [#4] feat : Create BaseTimeEntity
    User Post Comment 엔티티에서 공통적으로 생성일자와 수정일자라는 속성을 가지고 있습니다. 해당 속성을 BaseTimeEntity 에 @MappedSuperclass 를 통해서 공통적으로 상속받아 필드로 가질 수 있게 끔 하였고, @EntityListeners(AuditingEntityListener.class) 통해서 엔티티의 생성, 수정시 날짜를 자동 기입하도록 하였습니다. columnDefinition = "TIMESTAMP" 를 기록해준 이유는 데이터 베이스 마다 DateTime 으로 들어갈 수 있기 때문에 TIMESTAMP 를 통해 디비에 의존적이지 않게 하기 위해서 입니다.

---
+++ toylog/src/main/java/com/sdev/toylog/entity/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.sdev.toylog.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "CREATED_AT", columnDefinition = "TIMESTAMP")
+    private final LocalDateTime createDate = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(name = "UPDATED_AT", columnDefinition = "TIMESTAMP")
+    private LocalDateTime updateDate = LocalDateTime.now();
+
+}
