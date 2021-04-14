79ed1299f429c13112c157ee676f4e115def3e11
tmdgusya <tmdgusya@naver.com>
2021-04-14 10:11:04 +0900

    [#11] feature: User Domain Class 를 생성한다.

---
+++ toylog_backend/src/main/java/com/sdev/toylog/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sdev.toylog.repository;
+
+import com.sdev.toylog.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    List<User> findAll();
+
+}
