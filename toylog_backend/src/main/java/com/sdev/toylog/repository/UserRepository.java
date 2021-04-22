14afd3771a72a4a91f88099740fe150f07045a00
tmdgusya <tmdgusya@naver.com>
2021-04-22 20:48:35 +0900

    [#10] feat: User Login 기능 구현
    일단 Servlet Session 을 이용해 구현하였음, 후에 Redis 로 대체해야할 필요가 있다고 생각됨.

--- toylog_backend/src/main/java/com/sdev/toylog/repository/UserRepository.java
+++ toylog_backend/src/main/java/com/sdev/toylog/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.sdev.toylog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -13,5 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUserId(String userId);
 
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 
 }
--- toylog_backend/src/main/java/com/sdev/toylog/service/UserService.java
+++ toylog_backend/src/main/java/com/sdev/toylog/service/UserService.java
@@ -1,16 +1,19 @@
 package com.sdev.toylog.service;
 
+import com.sdev.toylog.dto.request.LoginSuccessResponse;
 import com.sdev.toylog.dto.request.UserCreateRequest;
+import com.sdev.toylog.dto.request.UserLoginRequest;
 import com.sdev.toylog.entity.User;
 import com.sdev.toylog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
+    private static final ModelMapper modelMapper = new ModelMapper();
 
     private final UserRepository userRepository;
     private final EncryptService encryptService;
@@ -27,6 +30,19 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public LoginSuccessResponse loginUser(UserLoginRequest userLoginRequest) throws IllegalAccessException {
+        //TODO 후에 레디스 도입 필요. 일단 Servlet Session 으로 진행
+        String encryptPassword = encryptService.encrypt(userLoginRequest.getPassword());
+
+        //TODO 일단 Custom 예외를 만들지 않았음, 주간 회의 필요
+        final User user = userRepository.findByEmail(userLoginRequest.getEmail()).orElseThrow(IllegalArgumentException::new);
+        if(!user.isMatchedPassword(encryptPassword)) {
+            //TODO 후에 Custom Exception 필요함
+            throw new IllegalAccessException("비밀번호가 틀렸습니다.");
+        }
+        return modelMapper.map(user, LoginSuccessResponse.class);
+    }
+
     public void checkDuplicatedUserId(String userId) {
         if (userRepository.existsByUserId(userId)) {
             throw new IllegalStateException("이미 존재하는 아이디 입니다");
@@ -38,4 +54,5 @@ public class UserService {
             throw new IllegalStateException("이미 존재하는 이메일 입니다");
         }
     }
+
 }
